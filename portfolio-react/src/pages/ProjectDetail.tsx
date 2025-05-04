9d84aefaff741f3c2012365c00df1ba55fc3b075
bryanlr96 <bryanlopezruiz1996@gmail.com>
2025-05-04 12:24:44 +0200

    actualizando carpetas

--- portfolio-react/src/pages/ProjectDetail.tsx
+++ portfolio-react/src/pages/ProjectDetail.tsx
@@ -10,6 +10,10 @@ export default function ProjectDetail() {
     const { slug } = useParams()
     const { state } = useGlobal()
 
+    console.log(slug)
+    const prueba = state.projects.find(project => project.slug?.current === '/' + slug)
+    console.log(prueba)
+    
     const project = getProjectBySlug(state.projects, slug || '')
     const { technologies } = state
     if (!project) return <h1>Projecto no encontrado</h1>
@@ -21,7 +25,12 @@ export default function ProjectDetail() {
                     <img src={urlFor(project.image).url()} alt="imagen portada del proyecto" className="w-[50%] my-20" />
                     <h1 className="text-4xl uppercase underline">{project.name}</h1>
                     <div className="w-1/2 my-10 gap-5 flex flex-col">
-                        <p>{project.description}</p>
+                        {/* <p>{project.description}</p> */}
+                        <div>
+                            {project.description.split('\n').map((line, idx) =>
+                                line.trim() && <p key={idx} className="mb-4">{line}</p>
+                            )}
+                        </div>
                         <h2>Tecnologias utilizadas:</h2>
                         <div className="w-full flex flex-wrap gap-5">
                             {project.technologies.map(technology => {
@@ -41,7 +50,7 @@ export default function ProjectDetail() {
                                 GitHub
                             </a>
                             <a href={project.link} className="py-2 w-[50%] bg-white text-black border-2 border-black font-bold flex items-center">
-                                <FaLink  className="mx-5"/>
+                                <FaLink className="mx-5" />
                                 Projecto
                             </a>
                         </div>
