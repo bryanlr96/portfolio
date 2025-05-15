072a9ef16de09029ebe4f949532569b4a9e2ef35
Bryan <bryanlopezruiz1996@gmail.com>
2025-05-15 17:38:16 +0200

    ajustando tamaño del projectItem

--- portfolio-react/src/components/ProjectItem.tsx
+++ portfolio-react/src/components/ProjectItem.tsx
@@ -7,7 +7,7 @@ type ProjectItemProps = {
 
 export default function ProjectItem({project} : ProjectItemProps) {
   return (
-    <Link to={project.slug.current} className="w-[80%] mx-auto">
+    <Link to={project.slug.current} className="w-[90%] mx-auto">
         <img src={urlFor(project.image).url()} alt="portada del proyecto"  className="w-full aspect-video"/>
         <div className="w-full bg-blue-500 text-white flex items-center justify-center h-[10vh]">
             <h3 className=" text-3xl">{project.name}</h3>
