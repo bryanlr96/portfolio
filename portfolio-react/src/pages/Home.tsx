c9a99f20c993e8ca2ae1f6860ccbda6f2934c923
Bryan <bryanlopezruiz1996@gmail.com>
2025-05-15 18:02:01 +0200

    ajustando el boton

--- portfolio-react/src/pages/Home.tsx
+++ portfolio-react/src/pages/Home.tsx
@@ -8,8 +8,8 @@ import { FaBars } from "react-icons/fa";
 export default function Home() {
   return (
     <>
-      <button className=" md:hidden absolute top-6 right-6 bg-blue-500">
-        <FaBars/>
+      <button className="md:hidden absolute top-6 right-6 bg-blue-500 p-4 rounded">
+        <FaBars className="text-white" />
       </button>
       <Header />
       <AboutMe />
@@ -17,8 +17,8 @@ export default function Home() {
         <h2 className="text-white text-4xl uppercase mb-[10vh]">Proyectos</h2>
         <ProjectsDisplay />
       </main>
-      <Contact/>
-      <Footer/>
+      <Contact />
+      <Footer />
     </>
   )
 }
