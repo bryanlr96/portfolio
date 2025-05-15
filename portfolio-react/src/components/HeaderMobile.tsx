4c755550f0ec0585d25679b05d72bb06a4ee76d8
Bryan <bryanlopezruiz1996@gmail.com>
2025-05-15 18:15:43 +0200

    usando el setheader para que me haga el deploy

--- portfolio-react/src/components/HeaderMobile.tsx
+++ portfolio-react/src/components/HeaderMobile.tsx
@@ -5,10 +5,11 @@ type HeaderMobileProps = {
     setHeaderMobileVisible: Dispatch<React.SetStateAction<boolean>>
 }
 
-export default function HeaderMobile({headerMobileVisible,setHeaderMobileVisible} : HeaderMobileProps) {
+export default function HeaderMobile({headerMobileVisible, setHeaderMobileVisible} : HeaderMobileProps) {
   return (
     <div className={headerMobileVisible ? "md:hidden w-screen h-screen bg-black flex flex-col absolute top-0 left-0 z-50" : "hidden"}>
 
+        <div onClick={()=>{setHeaderMobileVisible(false)}}></div>
     </div>
   )
 }
