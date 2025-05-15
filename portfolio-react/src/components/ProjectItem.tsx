b32d81fada2e411c56a2d648a663a6c8f4ce40e3
Bryan <bryanlopezruiz1996@gmail.com>
2025-05-15 17:45:16 +0200

    ajustando titulo de projects

--- portfolio-react/src/components/ProjectItem.tsx
+++ portfolio-react/src/components/ProjectItem.tsx
@@ -7,10 +7,10 @@ type ProjectItemProps = {
 
 export default function ProjectItem({project} : ProjectItemProps) {
   return (
-    <Link to={project.slug.current} className="w-[90%] mx-auto">
+    <Link to={project.slug.current} className="w-full mx-auto">
         <img src={urlFor(project.image).url()} alt="portada del proyecto"  className="w-full aspect-video"/>
-        <div className="w-full bg-blue-500 text-white flex items-center justify-center h-[10vh]">
-            <h3 className=" text-3xl">{project.name}</h3>
+        <div className="w-full bg-blue-500 text-white flex items-center justify-center">
+            <h3 className=" text-2xl py-3">{project.name}</h3>
         </div>
     </Link>
   )
