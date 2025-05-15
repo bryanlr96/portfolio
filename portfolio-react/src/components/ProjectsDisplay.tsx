3011511a0bab15cce7e7ac4c80b8a321752f7bb2
Bryan <bryanlopezruiz1996@gmail.com>
2025-05-15 17:35:56 +0200

    probando a separar mas los projects

--- portfolio-react/src/components/ProjectsDisplay.tsx
+++ portfolio-react/src/components/ProjectsDisplay.tsx
@@ -5,7 +5,7 @@ export default function ProjectsDisplay() {
     const { state } = useGlobal()
     const { projects } = state
     return (
-        <section className="w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 scroll-mt-75 gap-5" id="projects">
+        <section className="w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 scroll-mt-75 gap-8" id="projects">
         {
             (projects.length > 0) ? (
                 projects.map(project => (
