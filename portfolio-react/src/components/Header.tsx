c3b270cc9830ae713ceed8d2729c139e1142d48a
Bryan <bryanlopezruiz1996@gmail.com>
2025-05-15 17:49:14 +0200

    mejorando los links del header

--- portfolio-react/src/components/Header.tsx
+++ portfolio-react/src/components/Header.tsx
@@ -5,13 +5,13 @@ export default function Header() {
     <header className="sticky top-0 left-0 bg-blue-500 w-full h-[10vh] hidden md:flex flex-row items-center md:text-2xl uppercase text-white font-bold">
       <ul className="flex  gap-10 w-[80%] mx-auto justify-evenly lg:justify-end items-center">
         <li>
-          <a href="#about">sobre mi</a>
+          <a href="#about" className="hover:underline">sobre mi</a>
         </li>
         <li>
-          <a href="#projects">projectos</a>
+          <a href="#projects"  className="hover:underline">projectos</a>
         </li>
         <li>
-          <a href="#contact">contacto</a>
+          <a href="#contact"  className="hover:underline">contacto</a>
         </li>
       </ul>
     </header>
