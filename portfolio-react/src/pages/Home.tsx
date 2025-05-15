a95feb3632db28659dc343ec4b6796b70319872a
Bryan <bryanlopezruiz1996@gmail.com>
2025-05-15 17:58:22 +0200

    añadiendo boton para la version mobile

--- portfolio-react/src/pages/Home.tsx
+++ portfolio-react/src/pages/Home.tsx
@@ -3,10 +3,14 @@ import Contact from "../components/Contact"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
 import ProjectsDisplay from "../components/ProjectsDisplay"
+import { FaBars } from "react-icons/fa";
 
 export default function Home() {
   return (
     <>
+      <button className=" md:hidden absolute top-6 right-6 bg-blue-500">
+        <FaBars/>
+      </button>
       <Header />
       <AboutMe />
       <main className="w-full min-h-[100vh] bg-[#303841] flex flex-col items-center py-[10vh]">
