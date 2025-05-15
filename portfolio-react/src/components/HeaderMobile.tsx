b2966c516ad16e0cf6174deb237efc9a344edd23
Bryan <bryanlopezruiz1996@gmail.com>
2025-05-15 18:12:25 +0200

    añadiendo el header mobile

---
+++ portfolio-react/src/components/HeaderMobile.tsx
@@ -0,0 +1,14 @@
+import { Dispatch } from "react"
+
+type HeaderMobileProps = {
+    headerMobileVisible: boolean,
+    setHeaderMobileVisible: Dispatch<React.SetStateAction<boolean>>
+}
+
+export default function HeaderMobile({headerMobileVisible,setHeaderMobileVisible} : HeaderMobileProps) {
+  return (
+    <div className={headerMobileVisible ? "md:hidden w-screen h-screen bg-black flex flex-col absolute top-0 left-0 z-50" : "hidden"}>
+
+    </div>
+  )
+}
--- portfolio-react/src/pages/Home.tsx
+++ portfolio-react/src/pages/Home.tsx
@@ -1,16 +1,20 @@
+import { useState } from "react"
 import AboutMe from "../components/AboutMe"
 import Contact from "../components/Contact"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
+import HeaderMobile from "../components/HeaderMobile"
 import ProjectsDisplay from "../components/ProjectsDisplay"
 import { FaBars } from "react-icons/fa";
 
 export default function Home() {
+  const [headerMobileVisible, setHeaderMobileVisible] = useState(false)
   return (
     <>
-      <button className="md:hidden fixed top-6 right-6 bg-blue-500 p-4 rounded cursor-pointer">
+      <button className="md:hidden fixed top-6 right-6 bg-blue-500 p-4 rounded cursor-pointer" onClick={() => {setHeaderMobileVisible(true)}}>
         <FaBars className="text-white" />
       </button>
+      <HeaderMobile headerMobileVisible={headerMobileVisible}  setHeaderMobileVisible={setHeaderMobileVisible}/>
       <Header />
       <AboutMe />
       <main className="w-full min-h-[100vh] bg-[#303841] flex flex-col items-center py-[10vh]">
