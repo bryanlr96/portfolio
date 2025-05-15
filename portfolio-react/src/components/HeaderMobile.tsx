9e4dee2b65fc35e332e451ccf324ca50d6fe7dac
Bryan <bryanlopezruiz1996@gmail.com>
2025-05-15 18:21:04 +0200

    añadiendo los links

--- portfolio-react/src/components/HeaderMobile.tsx
+++ portfolio-react/src/components/HeaderMobile.tsx
@@ -5,11 +5,21 @@ type HeaderMobileProps = {
     setHeaderMobileVisible: Dispatch<React.SetStateAction<boolean>>
 }
 
-export default function HeaderMobile({headerMobileVisible, setHeaderMobileVisible} : HeaderMobileProps) {
-  return (
-    <div className={headerMobileVisible ? "md:hidden w-screen h-screen bg-black flex flex-col absolute top-0 left-0 z-50" : "hidden"}>
-
-        <div onClick={()=>{setHeaderMobileVisible(false)}}></div>
-    </div>
-  )
+export default function HeaderMobile({ headerMobileVisible, setHeaderMobileVisible }: HeaderMobileProps) {
+    return (
+        <div className={headerMobileVisible ? "md:hidden w-screen h-screen bg-black/80 flex flex-col absolute top-0 left-0 z-50" : "hidden"}>
+            <ul className="w-full bg-blue-500 text-white">
+                <li className="h-[46px] border-b-1 border-b-white" onClick={() => {setHeaderMobileVisible(false)}}>
+                    <a href="#about" className="hover:underline">sobre mi</a>
+                </li>
+                <li className="h-[46px] border-b-1 border-b-white" onClick={() => {setHeaderMobileVisible(false)}}>
+                    <a href="#projects" className="hover:underline">projectos</a>
+                </li>
+                <li className="h-[46px] border-b-1 border-b-white" onClick={() => {setHeaderMobileVisible(false)}}>
+                    <a href="#contact" className="hover:underline">contacto</a>
+                </li>
+            </ul>
+            <div onClick={() => { setHeaderMobileVisible(false) }}></div>
+        </div>
+    )
 }
