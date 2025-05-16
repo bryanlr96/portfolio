e10888ffc0b1ecad49e9e9406d65424b5630f512
Bryan <bryanlopezruiz1996@gmail.com>
2025-05-16 16:17:02 +0200

    ajuste del tamaño de la imagen

--- portfolio-react/src/pages/ProjectDetail.tsx
+++ portfolio-react/src/pages/ProjectDetail.tsx
@@ -19,7 +19,7 @@ export default function ProjectDetail() {
             <main className="bg-[#303841]  w-full min-h-[80vh]">
                 <section className="w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 items-center text-white">
                     <div className=" w-full flex flex-col my-10 md:my-20">
-                        <img src={urlFor(project.image).url()} alt="imagen portada del proyecto" className="w-full md:w-[60%] lg:w-[50%] mx-auto" />
+                        <img src={urlFor(project.image).url()} alt="imagen portada del proyecto" className="w-full" />
                         <h2 className="my-5">Tecnologias utilizadas:</h2>
                         <div className="w-full flex flex-wrap gap-3 justify-center sm:justify-start">
                             {project.technologies.map(technology => {
